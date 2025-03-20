f54aaba81897349d8b01bbe7b877cf8ff0d19e3c
Raj_Patel_7807 <rajpatel7807@gmail.com>
2025-03-20 11:55:10 +0530

    Files Added

---
+++ TempCodes/B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+#define endl '\n'
+
+void solve() {
+
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("D:/Code_PlayGround/C++/Input.txt", "r", stdin);
+        freopen("D:/Code_PlayGround/C++/Output.txt", "w", stdout);
+        freopen("D:/Code_PlayGround/C++/Debug.txt", "w", stderr);
+    #endif
+
+    ll tt; cin >> tt;
+    while(tt--) solve();
+
+    return 0;
+}
